235031232fdc034f74e64868e408f7b2d093673f
Felix Cornelissen <felixcornelissen@gmail.com>
2020-05-07 17:53:14 +0200

    markdown

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -1,7 +1,7 @@
 <template>
   <article>
     <h1 class="title">{{ blogPost.title }}</h1>
-    <div>{{ blogPost.body }}</div>
+    <div v-html="$md.render(blogPost.body)"></div>
   </article>
 </template>
 
